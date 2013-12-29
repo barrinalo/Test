0ff6c850fed6259ac0d9ad75bb969b770d94aec5
barrinalo <sumbrella2001@yahoo.com.sg>
2013-12-29 21:39:39 +0800

    Skeleton Code

---
+++ Main.py
@@ -0,0 +1,11 @@
+#Emergence Project V1.0
+#Date Created 29th December 2013
+#No Idea what we are doing
+
+#Create general program loop
+Gameover = False;
+
+while not Gameover:
+    Player_Input = input("Press 'e' to exit");
+    if Player_Input == 'e':
+        Gameover = True;
